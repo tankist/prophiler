e92d43061751919547d7a710f8e5703c5cfd1ed6
Victor Gryshko <stalin1945@gmail.com>
2016-06-27 16:54:15 +0300

    Added RenderableInterface interface for self-renderable DataCollectors

---
+++ src/Fabfuel/Prophiler/DataCollector/RenderableInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fabfuel\Prophiler\DataCollector;
+
+/**
+ * Interface RenderableInterface
+ * @package Fabfuel\Prophiler\DataCollector
+ */
+interface RenderableInterface
+{
+
+    /**
+     * @return string
+     */
+    public function render();
+}
